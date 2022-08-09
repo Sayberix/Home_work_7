8a0d258da5c05ea4b86a409ed1c276515c74fed2
Maxim Konev <sayberix.ru@yandex.ru>
2022-08-10 00:10:54 +0700

    задача выполнена!

--- Program.cs
+++ Program.cs
@@ -1,40 +1,50 @@
 ﻿// Программа из введенного массива строк формирует массив строк, длина которого меньше либо равна 3 символам.
+#nullable disable
 
+Console.Write("Введите к-во элементов массива (не больше 10): ");
 int n = Convert.ToInt32(Console.ReadLine());
 if (n > 10)
 {
-    Console.WriteLine("К-во элементов массива должно быть меньше или равно 10! Программа завершает работу!")
+    Console.WriteLine("К-во элементов массива должно быть меньше или равно 10! Программа завершает работу!");
     return;
 }
 
 string[] array = new string[n];
 string[] resaultArray = new string[n];
 
-
-
+FillArray(array);
+Console.Write("Введенный массив: ");
+ShowArray(array);
+// удаляем пустые элементы массива
+Array.Resize(ref resaultArray, ArrayCheck(array, resaultArray));
+Console.Write("Результирующий массив: ");
+ShowArray(resaultArray);
 
 
 void FillArray(string[] fillableArray)
 {
     for (int i = 0; i < fillableArray.Length; i++)
     {
-        Console.WriteLine("Введите значение в виде строки " + i + " элемента массива:");
+        Console.WriteLine("Введите значение в виде строки " + (i + 1) + "-го элемента массива:");
         fillableArray[i] = Console.ReadLine();
     }
 }
 
-void ArrayCheck(string[] filledArray, string[] resaultArray)
+int ArrayCheck(string[] filledArray, string[] resaultArray)
 {
+    int count = 0;
     for (int i = 0; i < filledArray.Length; i++)
     {
-        if (filledArray[i].Length > 3)
-            Array.Clear(resaultArray, i, 1);
-        else
-            resaultArray[i] = filledArray[i];
+        if (filledArray[i].Length <= 3)
+        {
+            resaultArray[count] = filledArray[i];
+            count++;
+        }
     }
+    return (count);
 }
 
 void ShowArray(string[] displayedArray)
 {
-    Console.WriteLine($"[{String.Join(";",displayedArray)}]");
+    Console.WriteLine($"[{String.Join("; ", displayedArray)}]");
 }
