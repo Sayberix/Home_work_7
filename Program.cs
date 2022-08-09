8f27850ad5bd0dd729c8250d1d43b3c09f6e5923
Maxim Konev <sayberix.ru@yandex.ru>
2022-08-09 22:59:44 +0700

    созданы функции по заполнению массива, его проверки и вывода

--- Program.cs
+++ Program.cs
@@ -7,7 +7,34 @@ if (n > 10)
     return;
 }
 
-int [] array = new int [n];
-int [] resaultArray = new int [n];
+string[] array = new string[n];
+string[] resaultArray = new string[n];
 
 
+
+
+
+void FillArray(string[] fillableArray)
+{
+    for (int i = 0; i < fillableArray.Length; i++)
+    {
+        Console.WriteLine("Введите значение в виде строки " + i + " элемента массива:");
+        fillableArray[i] = Console.ReadLine();
+    }
+}
+
+void ArrayCheck(string[] filledArray, string[] resaultArray)
+{
+    for (int i = 0; i < filledArray.Length; i++)
+    {
+        if (filledArray[i].Length > 3)
+            Array.Clear(resaultArray, i, 1);
+        else
+            resaultArray[i] = filledArray[i];
+    }
+}
+
+void ShowArray(string[] displayedArray)
+{
+    Console.WriteLine($"[{String.Join(";",displayedArray)}]");
+}
